154fbeb9cac750741c703d03573e68360467342e
heireev <muhammadrifqi216@gmail.com>
2023-02-28 02:17:18 +0800

    -Menerapkan View Binding

--- app/src/main/java/com/dicoding/barvolume/MainActivity.kt
+++ app/src/main/java/com/dicoding/barvolume/MainActivity.kt
@@ -3,74 +3,78 @@ package com.dicoding.barvolume
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.dicoding.barvolume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-//    Ini untuk deklarasi variable
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
+// Delete For View Binding    //    Ini untuk deklarasi variable
+//    private lateinit var edtWidth: EditText
+//    private lateinit var edtHeight: EditText
+//    private lateinit var edtLength: EditText
+//    private lateinit var btnCalculate: Button
+//    private lateinit var tvResult: TextView
 
     //buat save data saat rotasi
     companion object {
         private const val STATE_RESULT = "state_result"
     }
 
+    //buat variable untuk binding
+    private lateinit var binding: ActivityMainBinding
+
     //Main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // >>> set code seperti ini untuk view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // <<<
 
-        //Mengambil data ke variable dengan ID
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
+// Delete For View Binding         //Mengambil data ke variable dengan ID
+//        edtWidth = findViewById(R.id.edt_width)
+//        edtHeight = findViewById(R.id.edt_height)
+//        edtLength = findViewById(R.id.edt_length)
+//        btnCalculate = findViewById(R.id.btn_calculate)
+//        tvResult = findViewById(R.id.tv_result)
 
-        btnCalculate.setOnClickListener(this)
+        binding.btnCalculate.setOnClickListener(this) // terapkan binding di depan ID
 
         //buat save data saat rotasi
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
+            binding.tvResult.text = result
         }
     }
 
     //buat save data saat rotasi
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
     }
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_calculate) {
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
+            val inputLength = binding.edtLength.text.toString().trim()
+            val inputWidth = binding.edtWidth.text.toString().trim()
+            val inputHeight = binding.edtHeight.text.toString().trim()
 
             var isEmptyFields = false
 
             if(inputLength.isEmpty()) {
                 isEmptyFields = true
-                edtLength.error = "Field ini tidak boleh kosong"
+                binding.edtLength.error = "Field ini tidak boleh kosong"
             }
             if(inputWidth.isEmpty()) {
                 isEmptyFields = true
-                edtWidth.error = "Field ini tidak boleh kosong"
+                binding.edtWidth.error = "Field ini tidak boleh kosong"
             }
             if(inputHeight.isEmpty()) {
                 isEmptyFields = true
-                edtHeight.error = "Field ini tidak boleh kosong"
+                binding.edtHeight.error = "Field ini tidak boleh kosong"
             }
             if (!isEmptyFields) {
                 val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+                binding.tvResult.text = volume.toString()
             }
         }
     }
